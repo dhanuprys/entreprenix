a788d683adc376d93adf2188ea3b26149b664468
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2025-02-14 12:20:42 +0800

    fix: add some table column

--- database/migrations/0001_01_01_000005_create_posts_table.php
+++ database/migrations/0001_01_01_000005_create_posts_table.php
@@ -14,31 +14,15 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->string('type', 10); // article, visual
-            $table->timestamps();
-        });
-
-        Schema::create('visual_posts', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
             $table->text('caption')->nullable();
             $table->timestamps();
         });
 
         Schema::create('post_assets', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('visual_post_id')->constrained('visual_posts')->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
             $table->string('asset_path');
             $table->string('type', 10);
-            $table->timestamps();
-        });
-
-        Schema::create('article_posts', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
-            $table->string('cover_path')->nullable();
-            $table->string('title');
-            $table->text('content');
             $table->timestamps();
         });
     }
@@ -49,8 +33,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('posts');
-        Schema::dropIfExists('visual_posts');
         Schema::dropIfExists('post_assets');
-        Schema::dropIfExists('article_posts');
     }
 };
