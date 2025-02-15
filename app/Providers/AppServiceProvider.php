58052bcc702656edee5da764c29913fd84e564f7
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2025-02-15 16:52:18 +0800

    fix: fix https issue on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Fix issue #14
+        if ($this->app->environment('production')) {
+            // Fix pagination error (no https) #17
+            $this->app['request']->server->set('HTTPS','on');
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
